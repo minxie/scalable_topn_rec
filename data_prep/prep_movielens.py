ded7406db6bb7426a838fdc670cb6413ef82f74c
minxie <minxie@cs.ubc.ca>
2013-09-24 18:04:05 -0700

    fix

--- data_prep/prep_movielens.py
+++ data_prep/prep_movielens.py
@@ -4,7 +4,9 @@ Prepare movielens data
 
 if __name__ == "__main__":
     data_file = '../data/ml-10M100K/ratings.dat'
+    damm_file = '../data/ml-10M100K/ratings.dat_mm'
     fid = open(data_file, 'r')
+    oid = open(damm_file, 'w')
 
     M = 0
     N = 0
@@ -16,6 +18,8 @@ if __name__ == "__main__":
     rating_list = []
     
     for line in fid:
+        if NNZ % 100000 == 0:
+            print NNZ.'\n'
         fields = line.split('::')
         if not fields[0] in dict_m:
             M += 1
@@ -26,9 +30,10 @@ if __name__ == "__main__":
         NNZ += 1
         rating_list.append([fields[0], fields[1], fields[2]])
 
-    fid.write('%%MatrixMarket matrix coordinate real general')
-    fid.write(str(M) + ' ' + str(N) + ' ' + str(NNZ) + '\n')
+    oid.write('%%MatrixMarket matrix coordinate real general')
+    oid.write(str(M) + ' ' + str(N) + ' ' + str(NNZ) + '\n')
     for rating in rating_list:
-        fid.write(rating[0] + ' ' + rating[1] + ' ' + rating[2] + '\n')
+        oid.write(rating[0] + ' ' + rating[1] + ' ' + rating[2] + '\n')
 
     fid.close()
+    oid.close()
