f573aba1ac6e78b98da6915f8e4265cb6218baec
Min Xie <minxie@dhcp-128-189-73-239.ubcsecure.wireless.ubc.ca>
2013-10-06 14:28:39 -0700

    fix

--- src/latent_model.py
+++ src/latent_model.py
@@ -5,8 +5,10 @@ training and testing, those should be defined in a separate class.
 
 import numpy as np
 
-
 class LatentModel:
+    minval = 1e-100
+    maxval = 1e100
+    
     def __init__(self, params):
         self._params = params
         # Randomly initiate matrixes with values from 0 to 1
@@ -20,4 +22,7 @@ class LatentModel:
 
     '''Predict score for user i(0:) and item j(0:)'''
     def predict(self, i, j):
-        return self.P[i,].dot(self.Q[j,])
+        pred_rating = self.P[i,].dot(self.Q[j,])
+        pred_rating = max(pred_rating, minval)
+        pred_rating = min(pred_rating, maxval)
+        return pred_rating
