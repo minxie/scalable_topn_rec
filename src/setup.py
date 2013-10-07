2d15e2d70abcdf5addb38b74e271a637a0051124
minxie <minxie@cs.ubc.ca>
2013-10-07 14:55:46 -0700

    fix

--- src/setup.py
+++ src/setup.py
@@ -8,5 +8,6 @@ setup(
     cmdclass = {'build_ext': build_ext}, include_dirs = [numpy.get_include()],
     ext_modules=[Extension("latent_model_cython", ["latent_model_cython.pyx"]),
                  Extension("mf_machine_cython", ["mf_machine_cython.pyx"]),
-                 Extension("sgd_mf_machine_cython", ["sgd_mf_machine_cython.pyx"])]
+                 Extension("sgd_mf_machine_cython", ["sgd_mf_machine_cython.pyx"]),
+                 Extension("sgd_cython", ["sgd_cython.pyx"])]
 )
