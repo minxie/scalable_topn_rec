66c7afb0b62888271c5a858cd449cdbc527ca4a7
minxie <minxie@cs.ubc.ca>
2013-10-06 22:58:29 -0700

    fix

--- src/sgd_mf_machine.py
+++ src/sgd_mf_machine.py
@@ -35,7 +35,14 @@ class SGDMachine(MFMachine):
                 try:
                     err = obsv - self._model.predict(user, item)
                     rmse_err += err * err
-                    print err
+                    if (abs(err) > 100):
+                        print user
+                        print item
+                        print obsv
+                        print err
+                        print self._model.P[user, ]
+                        print self._model.Q[item, ]
+                        input("Debug:")
 
                     self._model.P[user, ] += sgd_gamma * (err * self._model.Q[item, ]
                                                           - sgd_lambda * self._model.P[user, ])
