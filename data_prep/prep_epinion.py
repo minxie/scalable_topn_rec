21d909ba79ecd06023bdd830f68cbdc7324ea6de
Min Xie <minxie@dhcp-128-189-73-101.ubcsecure.wireless.ubc.ca>
2013-10-08 21:13:21 -0700

    fix

--- data_prep/prep_epinion.py
+++ data_prep/prep_epinion.py
@@ -11,20 +11,15 @@ if __name__ == "__main__":
     oid = open(damm_file, 'w')
 
     M = 0
+    N = 0
     NNZ = 0
     dict_n = {}
     rating_list = []
 
     user_id = 0
-    file_number = 0
+    item_id = 0
     for data_file in data_files:
         fid = open(data_file, 'r')
-        itemid = -1
-
-        if file_number > 100:
-            break
-        else:
-            file_number += 1
 
         valid_user_flag = 1
         for line in fid:
@@ -38,19 +33,22 @@ if __name__ == "__main__":
                 user_id += 1
                 valid_user_flag = 0
 
-            # if not fields[0] in dict_m:
-            #    M += 1
-            #    dict_m[fields[0]] = M
-            # NNZ += 1
-            # rating_list.append([dict_m[fields[0]], itemid, fields[1]])
-            oid.write(data_file + '\n')
-            oid.write(str(user_id) + ' ' + fields[3] + ' ' + fields[7] + '\n')
-
-        # oid.write('%%MatrixMarket matrix coordinate real general\n')
-        # oid.write(str(M) + ' ' + str(len(data_files)) + ' ' + str(NNZ) + '\n')
-        # for rating in rating_list:
-            # oid.write(str(rating[0]) + ' ' + str(rating[1]) + ' ' + rating[2] + '\n')
+            if not fields[3] in dict_n:
+                N += 1
+                dict_n[fields[3]] = N
+                item_id = N
+            else:
+                item_id = dict_n[fields[3]]
+            NNZ += 1
+            rating_list.append(user_id, item_id, fields[7]])
+            # oid.write(data_file + '\n')
+            # oid.write(str(user_id) + ' ' + fields[3] + ' ' + fields[7] + '\n')
 
         fid.close()
+
+    oid.write('%%MatrixMarket matrix coordinate real general\n')
+    oid.write(str(user_id) + ' ' + str(N) + ' ' + str(NNZ) + '\n')
+    for rating in rating_list:
+        oid.write(str(rating[0]) + ' ' + str(rating[1]) + ' ' + rating[2] + '\n')
         
     oid.close()
