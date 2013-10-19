0f9b51bb1fa57192ef4fc9a4d0f9747a269ce852
Min Xie <minxie@dhcp-128-189-78-98.ubcsecure.wireless.ubc.ca>
2013-10-19 14:52:40 -0700

    fix

--- sgd_epinion.sh
+++ sgd_epinion.sh
@@ -2,6 +2,6 @@
 
 for d in '10' '20' '30' '40' '50'
 do 
-    python src/run_sgd.py -d 50 -m 46932 -n 100922 -l 1e-4 -g 1e-4 \
+    python src/run_sgd.py -d $d -m 46932 -n 100922 -l 1e-4 -g 1e-4 \
         -tr "data/epinion/ratings_epinion_mm" -te "data/xx.te" -rl "result/epinion_res"
 done
--- sgd_movielens.sh
+++ sgd_movielens.sh
@@ -2,6 +2,6 @@
 
 for d in '10' '20' '30' '40' '50'
 do 
-    python src/run_sgd.py -d 50 -m 69878 -n 10677 -l 1e-4 -g 1e-4 \
+    python src/run_sgd.py -d $d -m 69878 -n 10677 -l 1e-4 -g 1e-4 \
         -tr "data/ml-10M100K/ratings.dat_mm" -te "data/xx.te" -rl "result/movielens_res"
 done
