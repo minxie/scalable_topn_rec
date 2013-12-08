91f920e067695ec285910f6a84ac9dc5225108b3
Min Xie <minxie@dhcp-128-189-77-213.ubcsecure.wireless.ubc.ca>
2013-12-08 04:59:58 -0800

    fix

--- sgd_movielens.sh
+++ sgd_movielens.sh
@@ -2,6 +2,6 @@
 
 for d in '10' '20' '30' '40' '50'
 do 
-    python src/run_sgd.py -d $d -m 69878 -n 10677 -l 1e-4 -g 1e-4 -a 0.5 -b 0.01 -c 5 -N 10\
+    python src/run_sgd.py -d $d -m 69878 -n 10677 -l 1e-4 -g 1e-4 -a 0.4 -b 0.01 -c 5 -N 10\
         -tr "data/ml-10M100K/ratings.dat_mm" -te "data/xx.te" -rl "result/movielens_res_static"
 done
