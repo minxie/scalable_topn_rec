47a14646128e037b2dc5e11af42f044a555c615f
Min Xie <minxie@dhcp-128-189-75-61.ubcsecure.wireless.ubc.ca>
2013-12-12 17:12:30 -0800

    fix

--- sgd_epinion_topn_compare.sh
+++ sgd_epinion_topn_compare.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python src/top_compare.py -infile "result/epinion_res_static" -oufile "result/epinion_res_static_topn_compare"
+python src/topn_compare.py -infile "result/epinion_res_static" -oufile "result/epinion_res_static_topn_compare"
