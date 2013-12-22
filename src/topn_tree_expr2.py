6301ebe3429226f00ef5fa311ecd5771dba3e56c
Min Xie <minxie@vpn38.cs.ubc.ca>
2013-12-21 21:53:57 -0800

    fix

---
+++ src/topn_tree_expr2.py
@@ -0,0 +1,35 @@
+import argparse
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="")
+    parser.add_argument('-infile', nargs='?', dest='infile', default="", type=str,
+                        help='Input File location.')
+    parser.add_argument('-oufile', nargs='?', dest='oufile', default="", type=str,
+                        help='Output File location.')
+    args = parser.parse_args()
+
+    iid = open(args.infile, 'r')
+    h_counter_position = []
+    for i in xrange(10):
+        h_counter_position.append({})
+    h_counter_global = {}
+    
+    for line in iid:
+        line = line.rstrip()
+        fields = line.split()
+        cur_node = tree_root
+        for i in xrange(10):
+            item = int(fields[i])
+
+            if not item in h_counter_global:
+                h_counter_global[item] = 1
+
+            if not item in h_counter_position[i]:
+                h_counter_position[i][item] = 1
+
+    print(len(h_counter_global))
+    for i in xrange(10):
+        print(len(h_counter_position[i]))
+
+    iid.close()
---
+++ topn_expr2.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+python src/topn_tree_expr2.py -infile "result/epinion_res_static0"
