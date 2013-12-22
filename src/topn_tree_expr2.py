8d5139556c33cec32cb6882500f74a39df65766c
Min Xie <minxie@vpn38.cs.ubc.ca>
2013-12-21 22:29:55 -0800

    fix

--- src/topn_tree_expr2.py
+++ src/topn_tree_expr2.py
@@ -37,7 +37,7 @@ if __name__ == "__main__":
     #print(len(h_counter_global))
     #for i in xrange(10):
 
-    sorted_x = sorted(h_counter_position[i].iteritems(), key=operator.itemgetter(1))
+    sorted_x = sorted(h_counter_position[args.p].iteritems(), key=operator.itemgetter(1))
     for x in sorted_x:
         print x
 
