0ae69f8345d5bfb9e2028b8ef05847d91e4e7541
Min Xie <minxie@vpn38.cs.ubc.ca>
2013-12-21 20:46:46 -0800

    fix

--- topn_expr1.sh
+++ topn_expr1.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python src/topn_tree_expr.py -infile "result/epinion_res_static0" -oufile "result/epinion_res_static0_tree"
+python src/topn_tree_expr1.py -infile "result/epinion_res_static0" -oufile "result/epinion_res_static0_tree"
